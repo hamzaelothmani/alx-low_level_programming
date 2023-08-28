b2dae3a2dc42c053705af172b408235ff45251ca
hamzaelothmani <medelothmani481@gmail.com>
2023-08-28 15:23:06 -0700

    More singly linked lists#8

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a New Node into a Linked List at a Specified Position
+ /**
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
  * @head: Pointer to the First Node in the Linked List
  * @idx: Index Where the New Node is Added
  * @n: Data to Insert in the New Node
@@ -10,35 +12,35 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int s;
-	listint_t *newNode;
-	listint_t *temporary = *head;
+        unsigned int s;
+        listint_t *newNode;
+        listint_t *temporary = *head;
 
-	newNode = malloc(sizeof(listint_t));
-	if (!newNode || !head)
-		return (NULL);
+        newNode = malloc(sizeof(listint_t));
+        if (!newNode || !head)
+                return (NULL);
 
-	newNode->n = n;
-	newNode->next = NULL;
+        newNode->n = n;
+        newNode->next = NULL;
 
-	if (idx == 0)
-	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
+        if (idx == 0)
+        {
+                newNode->next = *head;
+                *head = newNode;
+                return (newNode);
+        }
 
-	for (s = 0; temporary && s < idx; s++)
-	{
-		if (s == idx - 1)
-		{
-			newNode->next = temporary->next;
-			temporary->next = newNode;
-			return (newNode);
-		}
-		else
-			temporary = temporary->next;
-	}
+        for (s = 0; temporary && s < idx; s++)
+        {
+                if (s == idx - 1)
+                {
+                        newNode->next = temporary->next;
+                        temporary->next = newNode;
+                        return (newNode);
+                }
+                else
+                        temporary = temporary->next;
+        }
 
-	return (NULL);
+        return (NULL);
 }
