6e581a4887f1cecafe140d90a9345a57e1b98b7f
hamzaelothmani <medelothmani481@gmail.com>
2023-08-28 15:24:17 -0700

    More singly linked lists#9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,45 @@
 #include "lists.h"
 
 /**
- /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
- * @head: Pointer to the First Node in the Linked List
- * @idx: Index Where the New Node is Added
- * @n: Data to Insert in the New Node
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: Pointer to the New Node, or NULL if the Operation Fails
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        unsigned int s;
-        listint_t *newNode;
-        listint_t *temporary = *head;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-        newNode = malloc(sizeof(listint_t));
-        if (!newNode || !head)
-                return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
 
-        newNode->n = n;
-        newNode->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-        if (idx == 0)
-        {
-                newNode->next = *head;
-                *head = newNode;
-                return (newNode);
-        }
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-        for (s = 0; temporary && s < idx; s++)
-        {
-                if (s == idx - 1)
-                {
-                        newNode->next = temporary->next;
-                        temporary->next = newNode;
-                        return (newNode);
-                }
-                else
-                        temporary = temporary->next;
-        }
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
 
-        return (NULL);
+	return (NULL);
 }
