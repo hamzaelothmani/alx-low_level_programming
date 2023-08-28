1cdabe41bca36513602058749cab8b123c8d0edd
hamzaelothmani <medelothmani481@gmail.com>
2023-08-28 15:26:45 -0700

    More singly linked lists#10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,45 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * Insert_nodeint_at_index - Inserts a new node in a linked list,
+ * At a given position
+ * @head: Pointer to the first node in the list
+ * @idx: Index where the new node is added
+ * @n: Data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+        unsigned int s;
+        listint_t *newNode;
+        listint_t *temporary = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
-		return (NULL);
+        newNode = malloc(sizeof(listint_t));
+        if (!newNode || !head)
+                return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+        newNode->n = n;
+        newNode->next = NULL;
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+        if (idx == 0)
+        {
+                newNode->next = *head;
+                *head = newNode;
+                return (newNode);
+        }
 
-	for (i = 0; temp && i < idx; i++)
-	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
-	}
+        for (s = 0; temporary && s < idx; s++)
+        {
+                if (s == idx - 1)
+                {
+                        newNode->next = temporary->next;
+                        temporary->next = newNode;
+                        return (newNode);
+                }
+                else
+                        temporary = temporary->next;
+        }
 
-	return (NULL);
+        return (NULL);
 }
